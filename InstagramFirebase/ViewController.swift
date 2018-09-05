863397e09b5c9c8ecf354a990f92f4aa23357ab0
riza satyabudhi <rizasatyabudhi@yahoo.com>
2018-09-05 16:04:25 +0700

    Initial Commit

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by Riza Ega Satyabudhi on 05/09/18.
+//  Copyright © 2018 Universitas Gadjah Mada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
